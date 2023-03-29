e943f00a1c5ed73ad5af0d1378455dd00a5a235a
ezurno <ezurno@gmail.com>
2023-03-29 21:31:35 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2480

--- main.js
+++ main.js
@@ -2,20 +2,13 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let hour = Number(input[0].split(" ")[0]);
-let minute = Number(input[0].split(" ")[1]);
+let number_1 = Number(input[0].split(" ")[0]);
+let number_2 = Number(input[0].split(" ")[1]);
+let number_3 = Number(input[0].split(" ")[2]);
 
-let timer = Number(input[1].split(" "));
-
-minute += timer;
-
-while (60 <= minute) {
-  hour += 1;
-  minute -= 60;
-
-  if (hour === 24) {
-    hour = 0;
-  }
-}
-
-console.log(hour + " " + minute);
+if (number_1 == number_2 && number_2 == number_3)
+  console.log(10000 + number_1 * 1000);
+else if (number_1 == number_2) console.log(1000 + number_1 * 100);
+else if (number_1 == number_3) console.log(1000 + number_1 * 100);
+else if (number_2 == number_3) console.log(1000 + number_2 * 100);
+else console.log(Math.max(number_1, number_2, number_3) * 100);
