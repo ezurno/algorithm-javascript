b1813415ad0131f9bbea33c9a7139599871145a4
ezurno <ezurno@gmail.com>
2023-03-29 21:20:36 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2525

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-// Mac, Linux 는 유닉스 기반이라 /dev/stdin 으로 인해 터미널 상에서 데이터값을 입력 할 수 있음
-// 하지만 window는 불가능하기 때문에 /input.txt 파일을 따로 만들어서 테스트 해주어야 함
 let input = fs.readFileSync(filePath).toString().split("\n");
-// fs-module 을 사용하여 입력값 받아오기
 
 let hour = Number(input[0].split(" ")[0]);
 let minute = Number(input[0].split(" ")[1]);
 
-if (minute < 45) {
-  hour -= 1;
-  minute += 15;
+let timer = Number(input[1].split(" "));
 
-  if (hour < 0) hour = 23;
-} else minute -= 45;
+minute += timer;
+
+while (60 <= minute) {
+  hour += 1;
+  minute -= 60;
+
+  if (hour === 24) {
+    hour = 0;
+  }
+}
 
 console.log(hour + " " + minute);
