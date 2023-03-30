96c88c52d6dbd53300053bafb242092bf0ca47d4
ezurno <ezurno@gmail.com>
2023-03-30 23:41:19 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2438

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ let number = Number(input[0].split(" ")[0]);
 // 문자열을 수로 변환 할때 일반적으로 parseInt 보다 Number가 더 빠르게 동작함
 let log = "";
 
-for (let i = 1; i <= 9; i++) {
-  log += `${number} * ${i} = ${number * i}\n`;
+for (let i = 0; i < number; i++) {
+  for (let j = 0; j <= i; j++) {
+    log += "*";
+  }
+  log += "\n";
 }
 
 console.log(log);
