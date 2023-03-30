e73df2b0f8c239739470c1ac436d9451add0350b
ezurno <ezurno@gmail.com>
2023-03-30 23:24:32 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2739

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 let number = Number(input[0].split(" ")[0]);
 // 문자열을 수로 변환 할때 일반적으로 parseInt 보다 Number가 더 빠르게 동작함
+let log = "";
 
-let sum = 0;
-
-while (0 < number) {
-  sum += number;
-  number -= 1;
+for (let i = 1; i <= 9; i++) {
+  log += `${number} * ${i} = ${number * i}\n`;
 }
-console.log(sum);
+
+console.log(log);
