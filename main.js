c8ba3b2debfa5cdcf2489a1a9b33dd4526811f5d
ezurno <ezurno@gmail.com>
2023-03-30 23:47:57 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/15552

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let number = Number(input[0].split(" ")[0]);
-// 문자열을 수로 변환 할때 일반적으로 parseInt 보다 Number가 더 빠르게 동작함
 let log = "";
 
-for (let i = 0; i < number; i++) {
-  for (let j = 0; j <= i; j++) {
-    log += "*";
-  }
-  log += "\n";
+for (let i = 1; i <= number; i++) {
+  let st = Number(input[i].split(" ")[0]);
+  let nd = Number(input[i].split(" ")[1]);
+
+  log += `${st + nd}\n`;
 }
 
 console.log(log);
