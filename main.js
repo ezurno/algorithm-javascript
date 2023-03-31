a9539d656c98d27d534c038fb90eefa988482cd7
ezurno <ezurno@gmail.com>
2023-03-31 21:09:39 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/10818
    
    used reduce()

--- main.js
+++ main.js
@@ -5,13 +5,9 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 let number = Number(input[0].split(" ")[0]);
 let log = "";
 
-let array = input[1].split(" ").map(Number);
-let max = -Infinity;
-let min = Infinity;
+let array = input[1].split(" ").map((x) => Number(x));
 
-for (let i = 0; i < number; i++) {
-  max = Math.max(max, array[i]);
-  min = Math.min(min, array[i]);
-}
+let max = array.reduce((st, nd) => Math.max(st, nd));
+let min = array.reduce((st, nd) => Math.min(st, nd));
 
 console.log(`${min} ${max}`);
