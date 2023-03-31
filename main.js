c88606c2d5023a068b66f67d8c1aea0e547e0914
ezurno <ezurno@gmail.com>
2023-03-31 22:42:44 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/4344

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let data = input.map((value) => Number(value));
-let mySet = new Set();
+let cycle = Number(input[0]);
+let log = "";
 
-for (let i = 0; i < 10; i++) {
-  mySet.add(data[i] % 42);
+for (let i = 1; i <= cycle; i++) {
+  let array = input[i].split(" ").map(Number);
+  let array_length = array[0];
+  let sum = 0;
+  let counter = 0;
+
+  for (let j = 1; j <= array_length; j++) {
+    sum += array[j];
+  }
+
+  avg = sum / array_length; // 평균 값
+
+  for (let i = 1; i <= array_length; i++) {
+    if (avg < array[i]) counter++;
+  }
+
+  log += `${((counter / array[0]) * 100).toFixed(3)}%\n`;
 }
 
-console.log(mySet.size);
+console.log(log);
