76fb5e860318fe4f797de8eb00bc9c240c79657d
ezurno <ezurno@gmail.com>
2023-03-31 20:14:00 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/10818

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 let number = Number(input[0].split(" ")[0]);
 let log = "";
 
-for (let i = 1; i <= number; i++) {
-  let st = Number(input[i].split(" ")[0]);
-  let nd = Number(input[i].split(" ")[1]);
+let array = input[1].split(" ").map(Number);
+let max = -Infinity;
+let min = Infinity;
 
-  log += `${st + nd}\n`;
+for (let i = 0; i < number; i++) {
+  max = Math.max(max, array[i]);
+  min = Math.min(min, array[i]);
 }
 
-console.log(log);
+console.log(`${min} ${max}`);
