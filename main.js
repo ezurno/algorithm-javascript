5b9ae6ca2144a86ca67427435f0f8ba7abb2937c
ezurno <ezurno@gmail.com>
2023-03-31 22:06:16 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/3052

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let data = input.map((x) => Number(x));
-let max = Math.max(...data);
-// Math.max 함수로 값만 가져와 사용
+let data = input.map((value) => Number(value));
+let mySet = new Set();
 
-let index = data.indexOf(max) + 1;
-// indexOf 로 인덱스 위치 찾기
+for (let i = 0; i < 10; i++) {
+  mySet.add(data[i] % 42);
+}
 
-console.log(max);
-console.log(index);
+console.log(mySet.size);
