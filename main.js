eba8a3811029c98b02d3884c758ce1fd226ccb09
ezurno <ezurno@gmail.com>
2023-04-01 12:56:26 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/11720

--- main.js
+++ main.js
@@ -3,13 +3,6 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let cycle = Number(input[0]);
-let array = input[1].split(" ").map(Number);
-let newArray = [];
+let number = input[1].toString().split("").map(Number);
 
-let M = array.reduce((st, nd) => Math.max(st, nd));
-
-for (let i = 0; i < cycle; i++) {
-  newArray.push((array[i] / M) * 100);
-}
-
-console.log(newArray.reduce((a, b) => a + b) / cycle);
+console.log(number.reduce((st, nd) => st + nd));
