abffcf46f4936ccdee4dfc3927a24324b8026c5e
ezurno <ezurno@gmail.com>
2023-04-01 14:18:34 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2908

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let cycle = Number(input[0]);
+let array = input[0].toString().split(" ").map(String);
 
-for (let i = 1; i <= cycle; i++) {
-  let line = input[i].split(" ").map(String);
-  let log = "";
+let newArray_1 = "";
+let newArray_2 = "";
 
-  for (let j = 0; j <= line[1].length; j++) {
-    log += line[1].charAt(j).repeat(Number(line[0]));
-  }
+let first = array[0];
+let second = array[1];
 
-  console.log(log);
+for (let i = 0; i < first.length; i++) {
+  newArray_1 += first.split("").reverse()[i];
 }
+
+for (let i = 0; i < second.length; i++) {
+  newArray_2 += second.split("").reverse()[i];
+}
+
+console.log(Math.max(Number(newArray_1), Number(newArray_2)));
