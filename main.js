239fb09cf9769ac22fc55d1750b0baa2e3460962
ezurno <ezurno@gmail.com>
2023-04-01 13:51:08 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2675

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let cycle = Number(input[0]);
-let number = input[1].toString().split("").map(Number);
 
-console.log(number.reduce((st, nd) => st + nd));
+for (let i = 1; i <= cycle; i++) {
+  let line = input[i].split(" ").map(String);
+  let log = "";
+
+  for (let j = 0; j <= line[1].length; j++) {
+    log += line[1].charAt(j).repeat(Number(line[0]));
+  }
+
+  console.log(log);
+}
