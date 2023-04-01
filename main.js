185fde7334458e01d36bc5b34e1476b44d4e2131
ezurno <ezurno@gmail.com>
2023-04-01 16:49:40 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1152

--- main.js
+++ main.js
@@ -2,26 +2,6 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-function checker(data) {
-  let set = new Set(data[0]);
+data = input[0].trim().split(" ");
 
-  for (let i = 0; i < data.length - 1; i++) {
-    if (data[i] != data[i + 1]) {
-      if (set.has(data[i + 1])) {
-        return false;
-      } else set.add(data[i + 1]);
-    }
-  }
-
-  return true;
-}
-
-let number = Number(input[0]);
-let sum = 0;
-
-for (let i = 1; i <= number; i++) {
-  let data = input[i];
-  if (checker(data)) sum += 1;
-}
-
-console.log(sum);
+console.log(data == "" ? 0 : data.length);
