33d2247952c5e80a32e78c16288b0adad77639f9
ezurno <ezurno@gmail.com>
2023-04-01 16:12:38 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1316

--- main.js
+++ main.js
@@ -2,20 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let array = input[0].toString().split(" ").map(String);
+function checker(data) {
+  let set = new Set(data[0]);
 
-let newArray_1 = "";
-let newArray_2 = "";
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data[i] != data[i + 1]) {
+      if (set.has(data[i + 1])) {
+        return false;
+      } else set.add(data[i + 1]);
+    }
+  }
 
-let first = array[0];
-let second = array[1];
-
-for (let i = 0; i < first.length; i++) {
-  newArray_1 += first.split("").reverse()[i];
+  return true;
 }
 
-for (let i = 0; i < second.length; i++) {
-  newArray_2 += second.split("").reverse()[i];
+let number = Number(input[0]);
+let sum = 0;
+
+for (let i = 1; i <= number; i++) {
+  let data = input[i];
+  if (checker(data)) sum += 1;
 }
 
-console.log(Math.max(Number(newArray_1), Number(newArray_2)));
+console.log(sum);
