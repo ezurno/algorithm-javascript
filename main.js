2ed67275057bdbeb7e62e8a09013f48766e7ce5d
ezurno <ezurno@gmail.com>
2023-04-04 20:42:39 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/2750

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-data = input[0].trim().split(" ");
+let array = [];
+let cycle = Number(input[0]);
 
-console.log(data == "" ? 0 : data.length);
+for (let i = 1; i <= cycle; i++) {
+  array.push(Number(input[i]));
+}
+
+// console.log(array);
+
+array.sort((a, b) => {
+  return a - b;
+});
+
+let log = "";
+
+for (let i = 0; i < array.length; i++) {
+  log += `${array[i]} \n`;
+}
+
+console.log(log);
