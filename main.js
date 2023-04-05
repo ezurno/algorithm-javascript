534d2a62ebf2cb62b29ef120aca86012da4838f7
ezurno <ezurno@gmail.com>
2023-04-05 21:33:28 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1181

--- main.js
+++ main.js
@@ -3,26 +3,30 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let cycle = Number(input[0]);
-let logs = "";
-
 let array = [];
 
 for (let i = 1; i <= cycle; i++) {
-  let xy = input[i].toString().split(" ").map(Number);
-
-  array.push({
-    x: xy[0],
-    y: xy[1],
-  });
+  let word = input[i];
+  array.push(word);
 }
 
+array = [...new Set(array)]; // 중복 값 Set 으로 정렬 후 다시 ... 로 풀어서 초기화
+
+// console.log(array);
+
 array.sort((a, b) => {
-  if (a.y != b.y) return a.y - b.y;
-  else return a.x - b.x;
+  if (a.length != b.length) return a.length - b.length;
+  else {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
+  }
 });
 
-array.forEach((value) => {
-  logs += `${value.x} ${value.y}\n`;
-});
+let logs = "";
+
+for (let i = 0; i < array.length; i++) {
+  logs += `${array[i]}\n`;
+}
 
 console.log(logs);
