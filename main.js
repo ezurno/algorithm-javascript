641d91741e44b95ac45211e1d23ca97b0e43c2a9
ezurno <ezurno@gmail.com>
2023-04-05 23:14:24 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1427

--- main.js
+++ main.js
@@ -2,23 +2,16 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let cycle = Number(input[0]);
-let array = [];
+let numbers = input[0].split("").map(Number);
 
-for (let i = 1; i <= cycle; i++) {
-  let [age, name] = input[i].split(" ");
-  array.push({ age: Number(age), name: name });
-}
-
-array.sort((a, b) => {
-  if (a.age != b.age) return a.age - b.age;
-  else return a.name - b.name;
+numbers.sort((a, b) => {
+  return b - a;
 });
 
 let logs = "";
 
-for (x of array) {
-  logs += `${x.age} ${x.name}\n`;
+for (value of numbers) {
+  logs += `${value}`;
 }
 
 console.log(logs);
