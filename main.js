8ef69590df9b771ec64f081947a8c073c7865782
ezurno <ezurno@gmail.com>
2023-04-05 23:09:11 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/10814

--- main.js
+++ main.js
@@ -3,28 +3,22 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let cycle = Number(input[0]);
-let array = input[1].split(" ").map(Number);
+let array = [];
 
-let newArray = [...new Set(array)]; // set 으로 풀어쓴 후 초기화
+for (let i = 1; i <= cycle; i++) {
+  let [age, name] = input[i].split(" ");
+  array.push({ age: Number(age), name: name });
+}
 
-// console.log(newArray);
-
-newArray.sort((a, b) => {
-  return a - b;
+array.sort((a, b) => {
+  if (a.age != b.age) return a.age - b.age;
+  else return a.name - b.name;
 });
-
-// console.log(newArray);
 
 let logs = "";
 
-let numbers = new Map();
-
-for (let i = 0; i < newArray.length; i++) {
-  numbers.set(newArray[i], i); // map 으로 key 와 value 값으로 세팅
-}
-
 for (x of array) {
-  logs += `${numbers.get(x)} `;
+  logs += `${x.age} ${x.name}\n`;
 }
 
 console.log(logs);
