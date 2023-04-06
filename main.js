3c727a5bea0ae7c83bf0bf04c941a7bd7e29f65a
ezurno <ezurno@gmail.com>
2023-04-06 20:37:46 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1541

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let cycle = Number(input[0]);
-let array = [];
+// 최솟값을 구하려면 최대한 많은 수를 뺄셈에 맞춰 괄호안에 넣어야 함
+//  a - (b + c) - (d + e + f) ...
 
-array = input[1].split(" ").map(Number);
+let delMin = input[0].split("-");
 
-array.sort((a, b) => a - b);
-// console.log(array);
+// console.log(delMin);
 
-let time = 0;
-let sum = 0;
+let logs = 0;
 
-for (let i = 0; i < cycle; i++) {
-  time += array[i];
-  sum += time;
+for (let i = 0; i < delMin.length; i++) {
+  let target = delMin[i].split("+").map(Number);
+  let sum = target.reduce((a, b) => a + b);
+
+  if (i == 0) logs += sum;
+  else logs -= sum;
 }
 
-console.log(sum);
+console.log(logs);
