00229c5b090a8c1acec9632e8fb28191777ad4f8
ezurno <ezurno@gmail.com>
2023-04-06 20:26:13 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/11399

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let [cycle, money] = input[0].split(" ").map(Number);
+let cycle = Number(input[0]);
 let array = [];
 
-for (let i = 1; i <= cycle; i++) {
-  array.push(Number(input[i]));
+array = input[1].split(" ").map(Number);
+
+array.sort((a, b) => a - b);
+// console.log(array);
+
+let time = 0;
+let sum = 0;
+
+for (let i = 0; i < cycle; i++) {
+  time += array[i];
+  sum += time;
 }
 
-let counter = 0;
-
-for (let i = cycle - 1; 0 <= i; i--) {
-  counter += parseInt(money / array[i]);
-  money %= array[i];
-}
-
-console.log(counter);
+console.log(sum);
