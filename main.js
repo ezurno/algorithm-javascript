6550ec0a53261385061e8ffca85c2f869d925717
ezurno <ezurno@gmail.com>
2023-04-08 00:16:08 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1946

--- main.js
+++ main.js
@@ -2,14 +2,35 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let S = Number(input[0]);
-// 1에서부터 자연수를 더하는게 가장 많은 수를 더하는 것과 같음
+let testCase = Number(input[0]);
+let line = 1;
 
-let sum = 0;
-let current = 0;
+for (let i = 0; i < testCase; i++) {
+  let cycle = Number(input[line]); // 새로 들어오는 값
+  let array = []; // testCase를 받을 array
 
-while (sum <= S) {
-  current += 1; // 현재 자연수
-  sum += current;
+  for (let j = line + 1; j <= line + cycle; j++) {
+    let recruit = input[j].split(" ").map(Number);
+    array.push(recruit);
+  }
+
+  array.sort((a, b) => {
+    return a[0] - b[0]; // 첫 번째 시험으로 순위 순으로 정렬
+  });
+
+  // console.log(array);
+
+  let count = 0;
+  let cutLine = Number.MAX_VALUE; // 최솟값을 구하기 위해 수 중 제일 큰 값을 부여
+
+  for (let [a, b] of array) {
+    // array 내에 있는 첫 시험 a, 두번째 시험 b로 분할
+    if (b < cutLine) {
+      cutLine = b; // 순위 별로 두번째 시험이 최솟 값 보다 적으면 cutLine 초기화
+      count += 1;
+    }
+  }
+
+  console.log(count);
+  line += cycle + 1;
 }
-console.log(current - 1);
