94444a66dca97a498512ec8ac3a138b9b362d39c
ezurno <ezurno@gmail.com>
2023-04-08 18:42:39 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/9009

--- main.js
+++ main.js
@@ -2,20 +2,35 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let data = input[1].split(" ").map(Number);
-let result = 0;
-let arrow = new Array(1000001).fill(0); // 각 높이의 화살 갯수
+let pibo = [];
+pibo.push(0);
+pibo.push(1);
 
-for (let height of data) {
-  if (0 < arrow[height]) {
-    // 해당 높이에 이미 쏜 화살이 있다면
-    arrow[height] -= 1; // 해당 화살이 아래로 떨어지므로 하나 뺌
-    arrow[height - 1] += 1; // 한칸 밑으로 떨어진 화살
-  } else {
-    // 해당 높이에 화살이 없다면
-    arrow[height - 1] += 1;
-    result += 1; // 화살 수 세기
+while (pibo[pibo.length - 1] < 1e9) {
+  pibo.push(pibo[pibo.length - 2] + pibo[pibo.length - 1]);
+} // 비교 할 피보나치 수열을 생성
+
+// console.log(pibo);
+
+let testCase = Number(input[0]);
+
+for (let i = 1; i <= testCase; i++) {
+  let n = Number(input[i]); // Data 값
+  let result = [];
+  let index = pibo.length - 1;
+
+  while (n > 0) {
+    if (n >= pibo[index]) {
+      n -= pibo[index];
+      result.push(pibo[index]);
+    }
+
+    index--;
   }
-}
 
-console.log(result);
+  let answer = "";
+  for (let i = result.length - 1; 0 <= i; i--) {
+    answer += result[i] + " ";
+  }
+  console.log(answer);
+}
