0070184e705014860ba43e97251f71d5ed8b8a33
ezurno <ezurno@gmail.com>
2023-04-08 14:46:09 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/13305
    - 곱의 합을 계산 할 땐 BigInt를 활용해야 함

--- main.js
+++ main.js
@@ -2,35 +2,16 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let testCase = Number(input[0]);
-let line = 1;
+let city = Number(input[0]);
+let km = input[1].split(" ").map(Number);
+let oil = input[2].split(" ").map(Number);
+let sum = BigInt(0); // 값이 커질 수 있기 때문에
+let array = [];
 
-for (let i = 0; i < testCase; i++) {
-  let cycle = Number(input[line]); // 새로 들어오는 값
-  let array = []; // testCase를 받을 array
-
-  for (let j = line + 1; j <= line + cycle; j++) {
-    let recruit = input[j].split(" ").map(Number);
-    array.push(recruit);
-  }
-
-  array.sort((a, b) => {
-    return a[0] - b[0]; // 첫 번째 시험으로 순위 순으로 정렬
-  });
-
-  // console.log(array);
-
-  let count = 0;
-  let cutLine = Number.MAX_VALUE; // 최솟값을 구하기 위해 수 중 제일 큰 값을 부여
-
-  for (let [a, b] of array) {
-    // array 내에 있는 첫 시험 a, 두번째 시험 b로 분할
-    if (b < cutLine) {
-      cutLine = b; // 순위 별로 두번째 시험이 최솟 값 보다 적으면 cutLine 초기화
-      count += 1;
-    }
-  }
-
-  console.log(count);
-  line += cycle + 1;
+for (let i = 0; i < km.length; i++) {
+  array.push(oil[i]);
+  let cheap = array.reduce((a, b) => Math.min(a, b)); // 제일 싼 기름 찾기
+  sum += BigInt(cheap) * BigInt(km[i]);
 }
+
+console.log(String(sum)); // 뒤에 붙는  n 제거
