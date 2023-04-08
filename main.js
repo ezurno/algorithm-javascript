63c9699e01ce3a28586c8729b09dea709a772178
ezurno <ezurno@gmail.com>
2023-04-08 15:45:05 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1931

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let city = Number(input[0]);
-let km = input[1].split(" ").map(Number);
-let oil = input[2].split(" ").map(Number);
-let sum = BigInt(0); // 값이 커질 수 있기 때문에
+let communicate = Number(input[0]);
 let array = [];
-
-for (let i = 0; i < km.length; i++) {
-  array.push(oil[i]);
-  let cheap = array.reduce((a, b) => Math.min(a, b)); // 제일 싼 기름 찾기
-  sum += BigInt(cheap) * BigInt(km[i]);
+for (let i = 1; i <= communicate; i++) {
+  let [start, end] = input[i].split(" ").map(Number);
+  array.push({ start: start, end: end });
 }
 
-console.log(String(sum)); // 뒤에 붙는  n 제거
+array.sort((a, b) => {
+  if (a.start != b.start) {
+    return a.end - b.end;
+  } else {
+    return a.start - b.start;
+  }
+});
+
+console.log(array);
+
+let current = 0;
+let counter = 1;
+
+for (let i = 1; i < communicate; i++) {
+  if (array[current].end <= array[i].start) {
+    current = i;
+    counter += 1;
+  }
+}
+
+console.log(counter);
