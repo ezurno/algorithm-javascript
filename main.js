3523119e1ab1cc3066a4393a7f9c77e3e6a43ab5
ezurno <ezurno@gmail.com>
2023-04-09 16:12:13 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/17609

--- main.js
+++ main.js
@@ -2,23 +2,39 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let [ball, chest] = input[0].split(" ").map(Number);
-let sum = 0;
-
-for (let i = 1; i <= chest; i++) {
-  sum += i;
+function palindrome(word) {
+  return word == word.split("").reverse().join(""); // 뒤집어서 새 배열을 만듦
 }
 
-if (ball < sum) {
-  // 공의 갯수가 부족한 경우
-  console.log(-1);
-} else {
-  // 공의 갯수가 충분한 경우
-  ball -= sum; // 한개씩은 다 담아야 하므로
+let testCase = Number(input[0]);
+for (let i = 1; i <= testCase; i++) {
+  let word = input[i].trim();
+  // 줄 넘기기가 되어 있기 때문에 trim()으로 제거 해줘야 정상 작동
 
-  if (ball % chest == 0) {
-    console.log(chest - 1);
-  } else {
-    console.log(chest);
+  if (palindrome(word)) console.log(0);
+  else {
+    // 회문이 아닌경우
+    let similar = false;
+    let length = word.length;
+    for (let j = 0; j < parseInt(length / 2); j++) {
+      // 절반으로 나눠서 비교
+      if (word[j] != word[length - j - 1]) {
+        // 맞지않는 index 값을 찾았을 경우
+        if (palindrome(word.slice(0, j) + word.slice(j + 1, length)))
+          similar = true;
+        // 해당 앞의 단어를 지워보고 검사, 맞다면 유사회문
+        if (
+          palindrome(
+            word.slice(0, length - j - 1) + word.slice(length - j, length)
+          )
+        )
+          // 해당 뒤의 단어를 지워보고 검사, 맞다면 유사회문
+          similar = true;
+        break;
+      }
+    }
+
+    if (similar) console.log(1);
+    else console.log(2);
   }
 }
