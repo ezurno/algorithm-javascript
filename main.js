a47a28da07308b4f9d1269cff3426874ebd6b590
ezurno <ezurno@gmail.com>
2023-04-09 15:08:57 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/19939

--- main.js
+++ main.js
@@ -2,35 +2,23 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let pibo = [];
-pibo.push(0);
-pibo.push(1);
+let [ball, chest] = input[0].split(" ").map(Number);
+let sum = 0;
 
-while (pibo[pibo.length - 1] < 1e9) {
-  pibo.push(pibo[pibo.length - 2] + pibo[pibo.length - 1]);
-} // 비교 할 피보나치 수열을 생성
+for (let i = 1; i <= chest; i++) {
+  sum += i;
+}
 
-// console.log(pibo);
+if (ball < sum) {
+  // 공의 갯수가 부족한 경우
+  console.log(-1);
+} else {
+  // 공의 갯수가 충분한 경우
+  ball -= sum; // 한개씩은 다 담아야 하므로
 
-let testCase = Number(input[0]);
-
-for (let i = 1; i <= testCase; i++) {
-  let n = Number(input[i]); // Data 값
-  let result = [];
-  let index = pibo.length - 1;
-
-  while (n > 0) {
-    if (n >= pibo[index]) {
-      n -= pibo[index];
-      result.push(pibo[index]);
-    }
-
-    index--;
+  if (ball % chest == 0) {
+    console.log(chest - 1);
+  } else {
+    console.log(chest);
   }
-
-  let answer = "";
-  for (let i = result.length - 1; 0 <= i; i--) {
-    answer += result[i] + " ";
-  }
-  console.log(answer);
 }
