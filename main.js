28be0653693bc8d3f5465967609bc4219e2c4820
ezurno <ezurno@gmail.com>
2023-04-13 14:44:55 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/7490

--- main.js
+++ main.js
@@ -2,35 +2,36 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let N = Number(input[0]);
+let testCase = Number(input[0]);
 let array = [];
+let N = 0;
 
-for (let i = 1; i <= N; i++) {
-  array.push(i);
-}
+function dfs(result, depth) {
+  if (depth == N - 1) {
+    let str = ""; // 현재 수식 문자열
+    for (let i = 0; i < N - 1; i++) str += array[i] + result[i];
+    str += array[N - 1] + "";
 
-let visited = new Array(N).fill(false);
-let selected = [];
-
-let answer = "";
-function dfs(array, depth) {
-  if (depth == N) {
-    let result = [];
-    for (let i of selected) result.push(i); // 최종 값을 selected 에서 가져옴
-    for (let x of result) answer += `${x} `;
-    answer += "\n";
-    return; // 함수 종료
+    current = eval(str.split(" ").join("")); // 공백을 제거 한 후 배열을 총 합
+    if (current == 0) console.log(str);
+    return;
   }
 
-  for (let i = 0; i < N; i++) {
-    if (visited[i]) continue;
-    selected.push(array[i]);
-    visited[i] = true;
-    dfs(array, depth + 1);
-    selected.pop();
-    visited[i] = false;
+  for (let x of [" ", "+", "-"]) {
+    result.push(x);
+    dfs(result, depth + 1);
+    result.pop();
   }
 }
 
-dfs(array, 0); // 0 에서 시작
-console.log(answer);
+for (let i = 1; i <= testCase; i++) {
+  N = Number(input[i]);
+  array = [];
+
+  for (let i = 1; i <= N; i++) {
+    array.push(i);
+  }
+
+  dfs([], 0);
+  console.log();
+}
