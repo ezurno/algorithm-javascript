75bda1e9cb060a5f32470a3311344ead36a18d96
ezurno <ezurno@gmail.com>
2023-04-13 18:44:11 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/15650

--- main.js
+++ main.js
@@ -2,36 +2,34 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let testCase = Number(input[0]);
+let [N, M] = input[0].split(" ").map(Number);
 let array = [];
-let N = 0;
+let checked = new Array(N).fill(false);
+let selected = [];
 
-function dfs(result, depth) {
-  if (depth == N - 1) {
-    let str = ""; // 현재 수식 문자열
-    for (let i = 0; i < N - 1; i++) str += array[i] + result[i];
-    str += array[N - 1] + "";
+for (let i = 1; i <= N; i++) {
+  array.push(i);
+}
 
-    current = eval(str.split(" ").join("")); // 공백을 제거 한 후 배열을 총 합
-    if (current == 0) console.log(str);
-    return;
+let log = "";
+
+function dfs(array, depth, start) {
+  if (depth == M) {
+    let result = [];
+    for (let i of selected) result.push(i);
+    for (let x of result) log += `${x} `;
+    log += "\n";
   }
 
-  for (let x of [" ", "+", "-"]) {
-    result.push(x);
-    dfs(result, depth + 1);
-    result.pop();
+  for (let i = start; i < N; i++) {
+    if (checked[i]) continue;
+    selected.push(array[i]);
+    checked[i] = true;
+    dfs(array, depth + 1, i + 1);
+    selected.pop();
+    checked[i] = false;
   }
 }
 
-for (let i = 1; i <= testCase; i++) {
-  N = Number(input[i]);
-  array = [];
-
-  for (let i = 1; i <= N; i++) {
-    array.push(i);
-  }
-
-  dfs([], 0);
-  console.log();
-}
+dfs(array, 0, 0);
+console.log(log);
