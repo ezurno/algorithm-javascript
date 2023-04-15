e931f301d4bd5ae83d25ab02218a4fabaa8faefd
ezurno <ezurno@gmail.com>
2023-04-15 20:14:23 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/15651

--- main.js
+++ main.js
@@ -4,32 +4,30 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 let [N, M] = input[0].split(" ").map(Number);
 let array = [];
-let checked = new Array(N).fill(false);
-let selected = [];
 
 for (let i = 1; i <= N; i++) {
   array.push(i);
 }
 
-let log = "";
+// let visited = new Array(N).fill(false);
+let selected = [];
 
-function dfs(array, depth, start) {
-  if (depth == M) {
+let log = "";
+function dfs(array, depth) {
+  if (M == depth) {
     let result = [];
     for (let i of selected) result.push(i);
     for (let x of result) log += `${x} `;
     log += "\n";
+    return;
   }
 
-  for (let i = start; i < N; i++) {
-    if (checked[i]) continue;
-    selected.push(array[i]);
-    checked[i] = true;
-    dfs(array, depth + 1, i + 1);
+  for (let i = 1; i <= array.length; i++) {
+    selected.push(i);
+    dfs(array, depth + 1);
     selected.pop();
-    checked[i] = false;
   }
 }
 
-dfs(array, 0, 0);
+dfs(array, 0);
 console.log(log);
