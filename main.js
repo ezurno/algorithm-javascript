6177bb319ee54da3881e60115a3e468a1afb696e
ezurno <ezurno@gmail.com>
2023-04-16 20:19:28 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/10971

--- main.js
+++ main.js
@@ -2,31 +2,49 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let [N, M] = input[0].split(" ").map(Number);
+let N = Number(input[0]);
 let array = [];
+let visited = new Array(N).fill(false);
 
+for (let i = 0; i <= N; i++) array.push([0]);
 for (let i = 1; i <= N; i++) {
-  array.push(i);
+  let line = input[i].split(" ").map(Number); // 각 라인 값 찢어서 다시 넣기
+  for (let j = 0; j < N; j++) array[i].push(line[j]);
 }
 
-// let visited = new Array(N).fill(false);
-let selected = [];
+let result = [];
+let minValue = 1e9;
 
-let log = "";
-function dfs(array, depth, start) {
-  if (M == depth) {
-    let result = [];
-    for (let i of selected) result.push(array[i]);
-    for (let x of result) log += `${x} `;
-    log += "\n";
-    return;
+function dfs(depth) {
+  // 1에서부터 출발 하므로 2부터 n 까지
+  if (depth == N - 1) {
+    let totalCost = 0;
+    let current = 1; // 1번 노드에서 출발
+
+    for (let i = 0; i < N - 1; i++) {
+      let next = result[i];
+      let cost = array[current][next];
+
+      if (cost == 0) return;
+      totalCost += cost;
+      current = next;
+    }
+
+    let cost = array[current][1]; // 다시 1로 돌아오는 것이 필수
+    if (cost == 0) return;
+    totalCost += cost;
+    minValue = Math.min(minValue, totalCost);
   }
 
-  for (let i = start; i < array.length; i++) {
-    selected.push(i);
-    dfs(array, depth + 1, i);
-    selected.pop();
+  for (let i = 2; i <= N; i++) {
+    if (visited[i]) continue;
+    result.push(i);
+    visited[i] = true;
+    dfs(depth + 1);
+    result.pop();
+    visited[i] = false;
   }
 }
-dfs(array, 0, 0);
-console.log(log);
+
+dfs(0);
+console.log(minValue);
