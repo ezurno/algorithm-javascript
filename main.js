5e3290a5889ad83987349853fe629d30244c29e7
ezurno <ezurno@gmail.com>
2023-04-17 19:04:23 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/9663

--- main.js
+++ main.js
@@ -2,37 +2,31 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-for (let i = 0; i < input.length; i++) {
-  let line = input[i].split(" ").map(Number);
+let N = Number(input[0]);
+let chess = []; // queen 이 놓인 위치를 적는 배열
+let counter = 0;
 
-  if (line[0] == 0) break;
-  else {
-    let N = line[0];
-    let array = line.slice(1);
-    visited = new Array(N).fill(false);
-    selected = [];
-    answer = "";
+function possible(x, y) {
+  for (let [a, b] of chess) {
+    if (a == x || b == y) return false;
+    // 해당하는 chess 판에 놓인 queen 의 위치 중 x, y 축이 같을 시 false
+    if (Math.abs(a - x) == Math.abs(b - y)) return false;
+    // 대각선에 놓여있을 시 false
+  }
 
-    dfs(array, 0, 0);
-    console.log(answer);
+  return true;
+}
+
+function dfs(row) {
+  if (row == N) counter += 1;
+
+  for (let i = 0; i < N; i++) {
+    if (!possible(row, i)) continue; // 조건을 만족하지 않았을 시
+    chess.push([row, i]); // 조건을 만족할 시 queen 을 놓음
+    dfs(row + 1); // 다음 행에 배치 (재귀함수 호출)
+    chess.pop();
   }
 }
 
-function dfs(array, depth, start) {
-  if (depth == 6) {
-    let result = [];
-    for (let i of selected) result.push(array[i]);
-    for (let x of result) answer += x + " ";
-    answer += "\n";
-    return;
-  }
-
-  for (let i = start; i < array.length; i++) {
-    if (visited[i]) continue;
-    selected.push(i);
-    visited[i] = true;
-    dfs(array, depth + 1, i + 1);
-    selected.pop(i);
-    visited[i] = false;
-  }
-}
+dfs(0);
+console.log(counter);
