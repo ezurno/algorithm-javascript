5ebcbfa59eead86a961e4a380516906140e817fd
ezurno <ezurno@gmail.com>
2023-04-17 17:51:22 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/6603

--- main.js
+++ main.js
@@ -2,43 +2,37 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let N = Number(input[0]);
-let array = [];
+for (let i = 0; i < input.length; i++) {
+  let line = input[i].split(" ").map(Number);
 
-for (let i = 1; i <= N; i++) {
-  let [x, y] = input[i].split(" ").map(Number);
-  array.push([x, y]);
+  if (line[0] == 0) break;
+  else {
+    let N = line[0];
+    let array = line.slice(1);
+    visited = new Array(N).fill(false);
+    selected = [];
+    answer = "";
+
+    dfs(array, 0, 0);
+    console.log(answer);
+  }
 }
 
-// console.log(array);
-
-let visited = new Array(N).fill(false);
-let result = [];
-let answer = 1e9;
-
-function dfs(depth, start) {
-  if (1 <= depth) {
-    let totalX = 1;
-    let totalY = 0;
-
-    for (let i of result) {
-      let [x, y] = array[i];
-      totalX *= x;
-      totalY += y;
-    }
-
-    answer = Math.min(answer, Math.abs(totalX - totalY));
+function dfs(array, depth, start) {
+  if (depth == 6) {
+    let result = [];
+    for (let i of selected) result.push(array[i]);
+    for (let x of result) answer += x + " ";
+    answer += "\n";
+    return;
   }
 
-  for (let i = start; i < N; i++) {
+  for (let i = start; i < array.length; i++) {
     if (visited[i]) continue;
+    selected.push(i);
     visited[i] = true;
-    result.push(i);
-    dfs(depth + 1, i + 1);
+    dfs(array, depth + 1, i + 1);
+    selected.pop(i);
     visited[i] = false;
-    result.pop();
   }
 }
-
-dfs(0, 0);
-console.log(answer);
