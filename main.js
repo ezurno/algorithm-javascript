597f3ce06335452ec3b48e11c3f693dee4463c7d
ezurno <ezurno@gmail.com>
2023-04-21 21:56:01 +0900

    SOL: quest solved
    https://www.acmicpc.net/problem/1240

--- main.js
+++ main.js
@@ -3,55 +3,30 @@ const { connected } = require("process");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let testCases = Number(input[0]);
-let line = 1;
+let [node, cycle] = input[0].split(" ").map(Number);
+let graph = [];
 
-for (let TC = 0; TC < testCases; TC++) {
-  let [X, Y, A] = input[line].split(" ").map(Number);
-  // x 는 x 축, y는 y 축, a 는 배추 갯수
-
-  let graph = []; // 각 값에 해당하는 그래프 생성
-
-  for (let i = 0; i < Y; i++) {
-    graph[i] = new Array(X);
-  }
-
-  // console.log(graph);
-
-  for (let i = 1; i <= A; i++) {
-    let [y, x] = input[line + i].split(" ").map(Number);
-    graph[x][y] = 1; // 배추의 위치를 1로 표현
-  }
-
-  // console.log(graph);
-
-  let counter = 0;
-
-  for (let i = 0; i < Y; i++) {
-    for (let j = 0; j < X; j++) {
-      if (dfs(graph, Y, X, i, j)) counter += 1; // 만약 배추탐색이 완료되면 갯수 ++
-    }
-  }
-
-  line += A + 1;
-  console.log(counter);
+for (let i = 1; i <= node; i++) graph[i] = []; // graph 생성
+for (let i = 1; i < node; i++) {
+  let [a, b, value] = input[i].split(" ").map(Number);
+  graph[a].push([b, value]);
+  graph[b].push([a, value]);
 }
 
-function dfs(graph, Y, X, i, j) {
-  if (i < 0 || j < 0 || Y <= i || X <= j) {
-    return false;
-  }
+// console.log(graph);
 
-  if (graph[i][j] == 1) {
-    graph[i][j] = -1;
+function dfs(a, dist) {
+  if (visited[a]) return;
+  visited[a] = true;
+  distance[a] = dist;
+  for (let [b, value] of graph[a]) dfs(b, dist + value);
+}
 
-    dfs(graph, Y, X, i - 1, j);
-    dfs(graph, Y, X, i, j - 1);
-    dfs(graph, Y, X, i + 1, j);
-    dfs(graph, Y, X, i, j + 1);
+for (let i = 0; i < cycle; i++) {
+  let [a, b] = input[node + i].split(" ").map(Number);
+  visited = new Array(node + 1).fill(false);
+  distance = new Array(node + 1).fill(-1);
 
-    return true;
-  }
-
-  return false;
+  dfs(a, 0);
+  console.log(distance[b]);
 }
